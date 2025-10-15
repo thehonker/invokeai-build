5343805389851da8be372509a52a545fcb468a35
Edwin Pers <pers.edwin@gmail.com>
2025-10-15 11:29:52 -0400

    fixup entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,10 +4,10 @@ echo "entrypoint.sh start"
 
 echo "activating venv at ${HOME}/invokeai"
 
-cd "${HOME}/invokeai/" || exit 1
+cd "${HOME}" || exit 1
 
-. "${HOME}/venv/bin/activate"
+source "${HOME}/venv/bin/activate"
 
 echo "launching invokeai with args: $@"
 
-exec python3 "${HOME}/invokeai/main.py" "$@"
+exec invokeai-web "${@}"
