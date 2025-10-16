b938b5297479cb5ff89846569f919bf970c061ea
Edwin Pers <pers.edwin@gmail.com>
2025-10-15 20:29:13 -0400

    setup handling of GPU_DRIVER to map TORCH_INDEX_URL

--- src/install-invokeai.sh
+++ src/install-invokeai.sh
@@ -6,12 +6,51 @@ export GPU_DRIVER=$1
 
 export DEBIAN_FRONTEND=noninteractive
 
+TORCH_PIP_PRE=""
+
+case $GPU_DRIVER in
+  cu126)
+    TORCH_INDEX_URL=https://download.pytorch.org/whl/cu126
+    ;;
+  cu128)
+    TORCH_INDEX_URL=https://download.pytorch.org/whl/cu128
+    ;;
+  cu130)
+    TORCH_INDEX_URL=https://download.pytorch.org/whl/cu130
+    ;;
+  rocm6.4)
+    TORCH_INDEX_URL=https://download.pytorch.org/whl/rocm6.4
+    ;;
+  rocm7.0)
+    TORCH_INDEX_URL=https://download.pytorch.org/whl/nightly/rocm7.0
+    TORCH_PIP_PRE="--pre"
+    ;;
+  cpu)
+    TORCH_INDEX_URL=https://download.pytorch.org/whl/cpu
+    ;;
+  *)
+    echo "Unsupported GPU driver: $GPU_DRIVER"
+    exit 1
+    ;;
+esac
+
 pipx install uv
 
-$HOME/.local/bin/uv venv --relocatable --prompt invoke --python 3.12 --python-preference only-managed .venv
+$HOME/.local/bin/uv venv --relocatable --prompt invoke --python 3.12 --python-preference only-managed $HOME/.venv
 
 . .venv/bin/activate
-$HOME/.local/bin/uv pip install --upgrade "invokeai @ $HOME/invokeai-source" --python 3.12 --python-preference only-managed --torch-backend=$GPU_DRIVER --force-reinstall
+
+$HOME/.local/bin/uv \
+  pip install \
+  "${TORCH_PIP_PRE}" \
+  --upgrade \
+    "invokeai @ $HOME/invokeai-source" \
+  --python 3.12 \
+  --python-preference only-managed \
+  --torch-backend $GPU_DRIVER \
+  --force-reinstall \
+  --index-url "${TORCH_INDEX_URL}"
+
 deactivate
 
 rm -rf $HOME/invokeai-source
