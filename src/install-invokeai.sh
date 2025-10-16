8eefff36abbcb2f96fb67404f1c0a5f15de5886e
Edwin Pers <pers.edwin@gmail.com>
2025-10-15 20:21:03 -0400

    we don't really need to rm the installers

--- src/install-invokeai.sh
+++ src/install-invokeai.sh
@@ -11,8 +11,7 @@ pipx install uv
 $HOME/.local/bin/uv venv --relocatable --prompt invoke --python 3.12 --python-preference only-managed .venv
 
 . .venv/bin/activate
-$HOME/.local/bin/uv pip install --upgrade "invokeai @ $HOME/invokeai" --python 3.12 --python-preference only-managed --torch-backend=$GPU_DRIVER --force-reinstall
+$HOME/.local/bin/uv pip install --upgrade "invokeai @ $HOME/invokeai-source" --python 3.12 --python-preference only-managed --torch-backend=$GPU_DRIVER --force-reinstall
 deactivate
 
-rm -rf $HOME/invokeai
-rm /install-invokeai.sh
+rm -rf $HOME/invokeai-source
