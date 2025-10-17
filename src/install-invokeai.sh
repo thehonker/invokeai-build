d04a91d26d3d72bd98ae0fd46b6e43c8934d7cd5
Edwin Pers <pers.edwin@gmail.com>
2025-10-16 22:19:16 -0400

    sort package list, add cmake, add bitsandbytes build

--- src/install-invokeai.sh
+++ src/install-invokeai.sh
@@ -13,20 +13,25 @@ TORCH_PIP_PRE=""
 case $GPU_DRIVER in
   cu126)
     TORCH_INDEX_URL=https://download.pytorch.org/whl/cu126
+    BNB_BACKEND="cuda"
     ;;
   cu128)
     TORCH_INDEX_URL=https://download.pytorch.org/whl/cu128
+    BNB_BACKEND="cuda"
     ;;
   cu130)
     TORCH_INDEX_URL=https://download.pytorch.org/whl/nightly/cu130
     TORCH_PIP_PRE="--pre"
+    BNB_BACKEND="cuda"
     ;;
   rocm6.3)
     TORCH_INDEX_URL=https://download.pytorch.org/whl/rocm6.3
+    BNB_BACKEND="hip"
     ;;
   rocm7.0)
     TORCH_INDEX_URL=https://download.pytorch.org/whl/nightly/rocm7.0
     TORCH_PIP_PRE="--pre"
+    BNB_BACKEND="hip"
     ;;
   xpu)
     TORCH_INDEX_URL=https://download.pytorch.org/whl/xpu
@@ -57,5 +62,15 @@ uv pip install \
   $TORCH_PIP_PRE \
   --torch-backend "${GPU_DRIVER}" \
   "$HOME/invokeai-source"
+
+# Install bitsandbytes from source
+# Clone bitsandbytes repo
+git clone https://github.com/bitsandbytes-foundation/bitsandbytes.git && cd $HOME/bitsandbytes
+
+# Compile & install
+# Use -DBNB_ROCM_ARCH="gfx90a;gfx942" to target specific gpu arch
+cmake $( [[ -n ${BNB_BACKEND} ]] && echo "-DCOMPUTE_BACKEND=${BNB_BACKEND}") -S .
+make
+uv pip install .
 
 deactivate
